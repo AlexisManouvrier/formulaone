acf99b75e0bfbfd17c2cfbaef7a275598ffdeb88
Hamidou.S <86967645+hamidousow@users.noreply.github.com>
2021-09-07 14:52:31 +0200

    nav config

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/index.scss'
 import Logo from './Logo';
+import './OpenMenu';
 
 const Navigation = () => {
     return (
@@ -15,12 +16,21 @@ const Navigation = () => {
             <aside id="menu">
                 <div className="container-banniere"></div>                
                 <ul>
-                    <li></li>
+                    <li> home </li>
+                    <li> about </li>
+                    <li> about </li>
+                    <li> about </li>
+                    <span className="social-icons">
+                        <i className="fab fa-facebook-f"></i>
+                        <i className="fab fa-twitter"></i>
+                        <i className="fab fa-instagram"></i>
+                        <i className="fab fa-youtube"></i>
+                    </span>
                 </ul>
             </aside>            
-        </header>            
-        </>
-    );
+        </header>                    
+        </>        
+    );   
 };
 
 export default Navigation;
