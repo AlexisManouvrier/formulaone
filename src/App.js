9852848948f15b2793ed7c351053f19b3c31c543
Fanny <65302971+Niny59118@users.noreply.github.com>
2021-09-08 16:32:05 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from 'react'
-import Slideshow from './Composant/caroussel.js'
+import React from 'react';
+import FormImage from './FormImage.jpg';
+import './App.css';
+import Form from './Components/Form';
 
 
-function Histoire() {
-    return(
-        <div className="Carrou">
-        <Slideshow/>
-        </div>
-    )
-    }
 
-export default Histoire
+function App() {
+  return (
+    <div className="App" width="50%">
+      <header id="Appheader" className="App-header">
+        <img src={FormImage} className="FormImage" width="100%" height="100%" alt="FormImage" />
+      </header>
+      <div id="backForm" className="blackdiv" width="50%">
+        <Form />
+      </div>
 
+    </div>
+  );
+}
 
+export default App;
