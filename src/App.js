5183d0e23b29da1e1471e6b6387ba8ebbfa92691
AlexisManouvrier <88370722+AlexisManouvrier@users.noreply.github.com>
2021-09-08 11:57:11 +0200

    Add files via upload
    Finished desktop / adding mobile - aesthetic not finished

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Slideshow from './Composant/caroussel.js'
+
+
+function Histoire() {
+    return(
+        <div className="Carrou">
+        <Slideshow/>
+        </div>
+    )
+    }
+
+export default Histoire
+
+
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Histoire from './App';
+import './histoireF1.scss';
+import 'react-slideshow-image/dist/styles.css';
+
+
+
+ReactDOM.render(<Histoire/>, document.getElementById('root'));
