327ed0d44719fe486fc0bc1e47e8c268ef4a6835
Hamidou.S <86967645+hamidousow@users.noreply.github.com>
2021-09-08 13:37:56 +0200

    menu animation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,52 +1,20 @@
 //import React from 'react';
 import '../styles/index.scss'
 import Logo from './Logo';
-import { NavLink } from 'react-router-dom';
-import React, { useState } from 'react';
 
-const Navigation = () => {
-    // open and close the menu by changing the booleans false to true 
-    // toggleMenu false = menu closed  
-    // toggleMenu true = menu open  
-    const [toggleMenu, setToggleMenu] = useState(false);
-    const [openAnimation, setOpenAnimation] = useState(false);
+//import React, { useState } from 'react';
+import AsideMenu from './AsideMenu';
 
-    //openAnimation = document.querySelector('#menu');
-
-    const toggleNav = () => {
-        // ! switch booleans value 
-        setToggleMenu(!toggleMenu)
-        setOpenAnimation(!openAnimation)
-    }
+const Navigation = () => {  
     
     return (
         <>        
         <header>
-            {toggleMenu && (
-                <aside id="menu" className="visible">
-                    <div className="container-banniere"></div>
-                    <div className="container-menu">               
-                        <ul>
-                            <NavLink to="/" className="navLink"> home </NavLink>
-                            <NavLink to="/news" className="navLink"> news </NavLink>
-                            <NavLink to="/member" className="navLink"> membres </NavLink>
-                            <NavLink to="/history" className="navLink"> histoire </NavLink>
-                            <NavLink to="/contact" className="navLink"> contact </NavLink>                    
-                            <div className="social-icons">
-                                <i className="fab fa-facebook-f"></i>
-                                <i className="fab fa-twitter"></i>
-                                <i className="fab fa-instagram"></i>
-                                <i className="fab fa-youtube"></i>
-                            </div>
-                        </ul>
-                    </div> 
-                </aside> 
-            )}
-
             <nav>        
                 <Logo />
-                <span className="btn-burger" onClick={toggleNav}></span>
-            </nav>                      
+                <span className="btn-burger"></span>
+            </nav> 
+            <AsideMenu />                     
         </header>                    
         </>        
     );   
