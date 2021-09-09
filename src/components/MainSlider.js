b6b55bd52f1495d850f35ee645d90597bb6ddbd7
Ariz Shahbaz <ariz.shahbaz@gmail.com>
2021-09-09 15:35:37 +0200

    Update MainSlider.js

--- src/components/MainSlider.js
+++ src/components/MainSlider.js
@@ -13,13 +13,39 @@ import "reactjs-popup/dist/index.css";
 function MainSlider() {
   const config = {
     className: "center",
-    // centerMode: true,
     dots: true,
     infinite: true,
     centerPadding: "60px",
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+    ],
   };
 
   const [settings] = useState(config);
@@ -29,29 +55,31 @@ function MainSlider() {
   const products = SliderData();
 
   return (
-    <div className="App">
+    <div className="slider">
       <Slider {...settings}>
         {products.map((x, i) => {
           return (
             <div key={i} className="img-card">
               <img className="img" src={x.img} alt="" />
-              <div class="card-body">
+              <div className="card-body">
                 <div className="card-title">{x.title}</div>
                 <div className="card-text">{x.text}</div>
+
+                <Popup
+                  trigger={<button className="btn">Read More</button>}
+                  position="center center"
+                  nested
+                >
+                  <div className="popup-content">
+                    <img className="img-detail" src={x.img} alt="" />
+                    <div className="wrap-detail">
+                      <div className="title-detail">{x.text}</div>
+                      <div className="text-detail">{x.long}</div>
+                      <div className="date-detail">{x.date}</div>
+                    </div>
+                  </div>
+                </Popup>
               </div>
-              {/* <button className="btn">Read More</button> */}
-              <Popup
-                trigger={<button className="btn">Read More</button>}
-                // position="center"
-              >
-                <div class="popup-body">
-                  <img className="img-detail" src={x.img} alt="" />
-                  {/* <div className="card-title">{x.title}</div> */}
-                  <div className="title-detail">{x.text}</div>
-                  <div className="text-detail">{x.long}</div>
-                  <div className="title-detail">{x.date}</div>
-                </div>
-              </Popup>
             </div>
           );
         })}
