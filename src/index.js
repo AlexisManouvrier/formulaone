08e3c960b392366928161a647535f29e491ffe6a
Ariz Shahbaz <ariz.shahbaz@gmail.com>
2021-09-09 15:36:54 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import NewsUtils from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <NewsUtils />
   </React.StrictMode>,
   document.getElementById("root")
 );
