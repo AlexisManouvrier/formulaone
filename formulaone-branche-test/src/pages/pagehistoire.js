ab3fdb90afb4b4ddc0f2d8a98c8b05acb49c2089
AlexisManouvrier <88370722+AlexisManouvrier@users.noreply.github.com>
2021-09-10 09:33:34 +0200

    Page Histoire

---
+++ formulaone-branche-test/src/pages/pagehistoire.js
@@ -0,0 +1,12 @@
+import Slideshow from './Composant/caroussel.js'
+
+
+function Histoire() {
+    return(
+        <div className="root">
+        <Slideshow/>
+        </div>
+    )
+    }
+
+export default Histoire
